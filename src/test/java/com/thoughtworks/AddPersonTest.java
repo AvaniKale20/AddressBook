825b172cc62c9857be1c188a637c0b1ad46cb42d
avanijayantkale <Avanikale20>
2019-09-26 23:15:18 +0530

    add two people and retrieve from address book

--- src/test/java/com/thoughtworks/AddPersonTest.java
+++ src/test/java/com/thoughtworks/AddPersonTest.java
@@ -40,4 +40,18 @@ class AddressBookTest {
 
     }
 
+    @Test
+    void givenWeAddTwoPerson_WhenRetrieveFromAddressBook_ThenReturnTwoPeople() {
+        Address address = new Address(" karave", "pune", "Maharashtra", 431415);
+        Person Bella = new Person(02, "Bella", "9098909890", address);
+        Address address1 = new Address(" Kothrud", "pune", "Maharashtra", 431415);
+        Person Khalesi = new Person(02, "Khalesi", "9098909890", address);
+        AddressBook addressBook = new AddressBook();
+
+        addressBook.add(Bella);
+        addressBook.add(Khalesi);
+
+        Assertions.assertEquals(2, addressBook.get().size());
+
+    }
 }
