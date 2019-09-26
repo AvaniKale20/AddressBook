4b08780f56d275e89d874db5b8c7285f1392855e
avanijayantkale <Avanikale20>
2019-09-26 22:45:17 +0530

    Add Behaviour of add person in address book

--- src/main/java/com/thoughtworks/AddressBook.java
+++ src/main/java/com/thoughtworks/AddressBook.java
@@ -1,26 +1,12 @@
 package com.thoughtworks;
 
-import java.io.*;
-import java.util.ArrayList;
-
-
 public class AddressBook {
+    Person person;
 
-    private Person person;
-
-
-    AddressBook(Person person) {
-        this.person = person;
+    AddressBook() {
     }
 
-    public boolean addPerson() throws IOException {
-        ArrayList<Person> person1 = new ArrayList<>();
-
-        person1.add(person);
-        person1.add(person);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/Users/avanijayantkale/Desktop/address.txt")));
-        writer.write(person1.toString());
-        writer.flush();
+    public boolean add(Person person) {
         return true;
 
     }
